414487d6b8fd18dcaf2dddd6f20619afb64c91b9
TridentMark <2411kjn@gmail.com>
2022-09-02 07:10:39 +0700

    feat: change logic

--- src/com/company/PointDistance.java
+++ src/com/company/PointDistance.java
@@ -11,8 +11,12 @@ class Point {
         this.y = y;
     }
 
-    public double getDistance() {
-        return Math.sqrt(x * x + y * y);
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
     }
 }
 
@@ -28,12 +32,19 @@ public class PointDistance {
         double minDistance = Double.MAX_VALUE;
         for (int i = 0; i < points.size(); i++) {
             for (int j = i + 1; j < points.size(); j++) {
-                double distance = points.get(i).getDistance();
+                double distance = getDistance(points.get(i), points.get(j));
                 if (distance < minDistance) {
                     minDistance = distance;
                 }
             }
         }
         return minDistance;
+    }
+
+    // find the distance between two points
+    public double getDistance(Point p1, Point p2) {
+        double xDiff = p1.getX() - p2.getX();
+        double yDiff = p1.getY() - p2.getY();
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
     }
 }
